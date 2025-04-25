559caacdf43b2ed2640ec0b712c3890383f517c5
Olawoore Damilola James <58667901+dammycute@users.noreply.github.com>
2025-04-25 10:23:24 +0100

    arrays and mapping

--- main.js
+++ main.js
@@ -1,77 +1,109 @@
-//1. Score Checker Function 
-function jambPassMark(score) {
-    if (score >= 180) {
-        console.log("Passed");
-    } else {
-        console.log("Failed");
-    }
+const drugs = [
+
+  { id: 1, name: "Amoxicillin", category: "Antibiotic", dosageMg: 500, isPrescriptionOnly: true, stock: 120, manufacturer: "Pfizer" },
+ 
+  { id: 2, name: "Paracetamol", category: "Analgesic", dosageMg: 1000, isPrescriptionOnly: false, stock: 200, manufacturer: "GSK" },
+ 
+  { id: 3, name: "Ibuprofen", category: "Analgesic", dosageMg: 400, isPrescriptionOnly: false, stock: 150, manufacturer: "Bayer" },
+ 
+  { id: 4, name: "Chloroquine", category: "Antimalarial", dosageMg: 250, isPrescriptionOnly: true, stock: 80, manufacturer: "Sanofi" },
+ 
+  { id: 5, name: "Ciprofloxacin", category: "Antibiotic", dosageMg: 500, isPrescriptionOnly: true, stock: 70, manufacturer: "Pfizer" },
+ 
+  { id: 6, name: "Loratadine", category: "Antihistamine", dosageMg: 10, isPrescriptionOnly: false, stock: 160, manufacturer: "Novartis" },
+ 
+  { id: 7, name: "Metformin", category: "Antidiabetic", dosageMg: 850, isPrescriptionOnly: true, stock: 140, manufacturer: "Teva" },
+ 
+  { id: 8, name: "Artemether", category: "Antimalarial", dosageMg: 20, isPrescriptionOnly: true, stock: 60, manufacturer: "Roche" },
+ 
+  { id: 9, name: "Aspirin", category: "Analgesic", dosageMg: 300, isPrescriptionOnly: false, stock: 180, manufacturer: "Bayer" },
+ 
+  { id: 10, name: "Omeprazole", category: "Antacid", dosageMg: 20, isPrescriptionOnly: true, stock: 90, manufacturer: "AstraZeneca" },
+ 
+  { id: 11, name: "Azithromycin", category: "Antibiotic", dosageMg: 250, isPrescriptionOnly: true, stock: 50, manufacturer: "Pfizer" },
+ 
+  { id: 12, name: "Cetirizine", category: "Antihistamine", dosageMg: 10, isPrescriptionOnly: false, stock: 110, manufacturer: "Novartis" },
+ 
+  { id: 13, name: "Insulin", category: "Antidiabetic", dosageMg: 100, isPrescriptionOnly: true, stock: 30, manufacturer: "Novo Nordisk" },
+ 
+  { id: 14, name: "Artemisinin", category: "Antimalarial", dosageMg: 100, isPrescriptionOnly: true, stock: 50, manufacturer: "GSK" },
+ 
+  { id: 15, name: "Codeine", category: "Analgesic", dosageMg: 30, isPrescriptionOnly: true, stock: 20, manufacturer: "Teva" },
+ 
+  { id: 16, name: "Vitamin C", category: "Supplement", dosageMg: 500, isPrescriptionOnly: false, stock: 300, manufacturer: "Nature’s Bounty" },
+ 
+  { id: 17, name: "Ranitidine", category: "Antacid", dosageMg: 150, isPrescriptionOnly: false, stock: 90, manufacturer: "Sanofi" },
+ 
+  { id: 18, name: "Doxycycline", category: "Antibiotic", dosageMg: 100, isPrescriptionOnly: true, stock: 40, manufacturer: "Pfizer" },
+ 
+  { id: 19, name: "Tramadol", category: "Analgesic", dosageMg: 50, isPrescriptionOnly: true, stock: 45, manufacturer: "Teva" },
+ 
+  { id: 20, name: "Folic Acid", category: "Supplement", dosageMg: 5, isPrescriptionOnly: false, stock: 250, manufacturer: "Nature’s Bounty" }
+ 
+ ];
+
+//  1 The filter of category with Antibiotic 
+const filterCategory = drugs.filter((drug) => drug.category === "Antibiotic");
+console.log(filterCategory);
+
+// 2. Returning Array of drugs in lowercase 
+const lowerCaseDrugs = drugs.map((drug) => drug.name.toLowerCase());
+console.log(lowerCaseDrugs);
+
+// 3. A function that accepts a category and returns all drugs in that category 
+const getDrugsByCategory = (category) => {
+  const filteredDrugs = drugs.filter((drug) => drug.category === category);
+  return filteredDrugs;
+};
+console.log(getDrugsByCategory("Supplement"));
+
+// 4. Logging each drugs and it's Manufacturer
+const logDrugs = (drugs) => {
+  drugs.forEach((drug) => {
+    console.log(`Drug: ${drug.name}, Manufacturer: ${drug.manufacturer}`);
+  });
+};
+logDrugs(drugs);
+
+// 5. Drugs that requires Prescription 
+const prescriptionOnlyDrugs = drugs.filter((drug) => drug.isPrescriptionOnly);
+console.log(prescriptionOnlyDrugs);
+
+
+// 6. Returning a new array, each item should follow the format: "Drug: [name] - [dosageMg]mg".
+const drugDetails = drugs.map((drug) => {
+  return `Drug: ${drug.name} - ${drug.dosageMg}mg`;
+});
+console.log(drugDetails);
+
+
+// 7. A function that returns an array of drug objects that have a stock less than 50
+function stockLessThan50(){
+  const drugsFilter = drugs.filter((drug) => drug.stock < 50);
+  return drugsFilter;
 }
+console.log(stockLessThan50()); 
 
-//2. Voting Eligibility Check Function
-function votingEligibility(age) {
-    if (age >= 18) {
-        return "You are eligible to vote"
-    } else {
-        return "Not eligible"
-    }
-}
 
-// 3. Grade Checker Function 
-function gradeChecker(score, name) {
-    if (score >= 90) {
-        return 'Excellent'
-    } else if (score >= 75) {
-        return "Good";
-    } else if (score >= 50) {
-        return "Average";
-    } else {
-        return "Fail";
-    }
-}
+// 8. Return all drugs that are not prescription-based-only 
+const drugsNotPrescriptionOnly = drugs.filter((drug) => !drug.isPrescriptionOnly);
+console.log(drugsNotPrescriptionOnly);
 
-// 4. ID and Age Checker for Access 
-function canAccess(hasID, isAbove18) {
-    return hasID && isAbove18 ? "Access granted" : "Access denied";
-  }
 
-  
-//5. Using For loop
-function checkScores(scores) {
-    for (let score of scores) {
-      if (score > 50) {
-        console.log("Pass");
-      } else {
-        console.log("Fail");
-      }
-    }
-  }
-  
-// 6. Using Arrow Function for subject pass
-const passedBothSubjects = (math, english) =>
-    math >= 50 && english >= 50 ? "Yes" : "No";
+// 9. Function that takes a manufacturer name and return how many drugs are from that company 
+const manufacturerCount = (manufacturer) => {
+  const count = drugs.filter((drug) => drug.manufacturer === manufacturer).length;
+  return count;
+};
+console.log(manufacturerCount("Pfizer"));
 
-// 7. Signup Details Checker
-function canSignUp(email, phone) {
-    if (email || phone) {
-      return "You can sign up";
-    } else {
-      return "Provide email or phone number";
-    }
+
+
+// 10. Use forEach() to count how many drugs are Analgesics.
+let analgesicCount = 0;
+drugs.forEach((drug) => {
+  if (drug.category === "Analgesic") {
+    analgesicCount++;
   }
-  
-//8. Input Validation Function
-function validateLogin(username, password) {
-    if (!username || !password) {
-      return "Invalid input";
-    } else {
-      return "Login successful";
-    }
-  }
-  
-// 9. Work Hours Checker Function
-function checkWorkType(hours) {
-    return hours >= 40 ? "Full-time" : "Part-time";
-  }
-  
-// 10. Using Arrow FUnction 
-const getLarger = (a, b) => (a > b ? a : b);
+});
+console.log(analgesicCount);
